020aef3f226f3199e2e292bb257f23e17602fd90
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2015-10-29 19:33:16 +0100

    Added functionality of omiting wrong files

--- ASO/Compresor/compresor.sh
+++ ASO/Compresor/compresor.sh
@@ -13,6 +13,22 @@ read -p "Selecciona una opción: " OPCION
 case $OPCION in
 	1)
 		read -p "Introduzca las rutas de los archivo que quieres comprimir:" ARCHIVOS
+		echo $ARCHIVOS
+		for TEST in $ARCHIVOS; do
+			if [ ! -f $TEST ]; then
+				INVALIDO="$INVALIDO $TEST"
+			fi
+		done
+		if [ ! "$INVALIDO" = '' ]; then
+			echo Las siguientes rutas no existen o son invalidas:
+			for OMITIDO in $INVALIDO; do
+				echo "\ \ $OMITIDO"
+				ARCHIVOS=`echo "$ARCHIVOS" | sed "s/$OMITIDO//g"`
+			done
+			echo "\nSe omitirán."
+		fi
+		echo $ARCHIVOS
+
 		read -p "Introduzca el nombre del contenedor: " CONTENEDOR
 		tar -zvcf $CONTENEDOR $ARCHIVOS
 		;;
