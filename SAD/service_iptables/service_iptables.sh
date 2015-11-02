44c5fe82578af25dbdf223744a6efe27a6cdd392
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2015-11-02 16:28:02 +0100

    Añadido un help

--- SAD/service_iptables/service_iptables.sh
+++ SAD/service_iptables/service_iptables.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 case $1 in 
-	stop)
-		iptables-restore < default_ipt.sh;;
-	start)
-		iptables-restore < iptables.conf;;
-	save)
-		iptables-save > iptables.conf;;
+	stop) iptables-restore < default_ipt.sh;;
+	start) iptables-restore < iptables.conf;;
+	save) iptables-save > iptables.conf;;
+	*) 
+		echo "service_iptables ACCION"
+		echo "    start:  Restablec la configuracion guardada."
+		echo "     stop:  Restablece la configuración por defecto"
+		echo "     save:  Guarda la configuración"
+		echo "     help:  Muestra la ayuda"
+esac
 
