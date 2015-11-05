58c2ec29aee3ccf083af6784d938bd148f902bdf
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2015-11-06 00:42:15 +0100

    Añadido echos y simplificación de la explicación de su uso.

--- SAD/service_iptables/service_iptables.sh
+++ SAD/service_iptables/service_iptables.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-case $1 in 
-	stop) iptables-restore < default_ipt.sh;;
-	start) iptables-restore < iptables.conf;;
-	save) iptables-save > iptables.conf;;
-	*) 
-		echo "service_iptables ACCION"
-		echo "    start:  Restablec la configuracion guardada."
-		echo "     stop:  Restablece la configuración por defecto"
-		echo "     save:  Guarda la configuración"
-		echo "     help:  Muestra la ayuda"
+case "$1" in
+	stop)
+		iptables-restore < iptables.default
+		echo "Restored default configuration."
+		;;
+	start)
+		iptables-restore < iptables.conf
+		echo "Loaded custom configuration."
+		;;
+	save)
+		iptables-save > iptables.conf
+		echo "Saved custom configuration."
+		;;
+	*)
+		echo "Usage: /etc/init.d/service_iptables.sh {start|stop|save}"
+		exit 1
 		;;
 esac
 
+exit 0
