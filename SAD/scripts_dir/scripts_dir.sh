462a17dba25cf555cef01a559cc8c5999cb7372e
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2015-11-06 22:24:07 +0100

    Añadido scripts_dir.sh
    scripts_dir.sh crea una carpeta llamada scripts si no existe, y la añade al PATH si no está añadido.

---
+++ SAD/scripts_dir/scripts_dir.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## You must add this part at the end of your ~/.bashrc file to create a directory called "scripts"
+## and add it to the PATH environment variable.
+
+if [ ! -d $HOME/scripts ]; then
+    mkdir $HOME/scripts
+fi
+
+DIRSCRIPT=`echo $(cd $HOME/scripts && pwd -P)`
+echo $PATH | grep -q $DIRSCRIPT
+if [ $? = 1 ]
+then
+    PATH ="${DIRSCRIPT}:$PATH"
+fi
